ad410c4ba614fa886508ea74ab9e28b06011c4da
Ravnholt7507 <Andreas.ravnholt99@gmail.com>
2022-09-13 19:19:04 +0200

    13/9

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,21 +29,176 @@ namespace Rynkeby
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new string[] {
+            "",
+            ""}, -1);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.nothing = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Event = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Currency = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Amount = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Dateandtime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Customername = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Phonenumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Comment = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Transactionid = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Transferid = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.PaymentPoint = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Myshopnumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Bankaccount = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Date = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Time = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.nothing,
+            this.Event,
+            this.Currency,
+            this.Amount,
+            this.Dateandtime,
+            this.Customername,
+            this.Phonenumber,
+            this.Comment,
+            this.Transactionid,
+            this.Transferid,
+            this.PaymentPoint,
+            this.Myshopnumber,
+            this.Bankaccount,
+            this.Date,
+            this.Time});
+            this.listView1.HideSelection = false;
+            listViewItem2.Checked = true;
+            listViewItem2.StateImageIndex = 1;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem2});
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1327, 661);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // nothing
+            // 
+            this.nothing.Text = "";
+            this.nothing.Width = 0;
+            // 
+            // Event
+            // 
+            this.Event.Text = "Event";
+            // 
+            // Currency
+            // 
+            this.Currency.Text = "Currency";
+            this.Currency.Width = 67;
+            // 
+            // Amount
+            // 
+            this.Amount.Text = "Amount";
+            this.Amount.Width = 64;
+            // 
+            // Dateandtime
+            // 
+            this.Dateandtime.Text = "Date and time";
+            this.Dateandtime.Width = 121;
+            // 
+            // Customername
+            // 
+            this.Customername.Text = "Customer name";
+            this.Customername.Width = 125;
+            // 
+            // Phonenumber
+            // 
+            this.Phonenumber.Text = "Phone number";
+            this.Phonenumber.Width = 134;
+            // 
+            // Comment
+            // 
+            this.Comment.Text = "Comment";
+            this.Comment.Width = 139;
+            // 
+            // Transactionid
+            // 
+            this.Transactionid.Text = "Transaction ID";
+            this.Transactionid.Width = 112;
+            // 
+            // Transferid
+            // 
+            this.Transferid.Text = "Transfer ID";
+            this.Transferid.Width = 71;
+            // 
+            // PaymentPoint
+            // 
+            this.PaymentPoint.Text = "Payment Point";
+            this.PaymentPoint.Width = 91;
+            // 
+            // Myshopnumber
+            // 
+            this.Myshopnumber.Text = "My-shop number";
+            this.Myshopnumber.Width = 95;
+            // 
+            // Bankaccount
+            // 
+            this.Bankaccount.Text = "Bank Account";
+            this.Bankaccount.Width = 85;
+            // 
+            // Date
+            // 
+            this.Date.Text = "Date";
+            this.Date.Width = 46;
+            // 
+            // Time
+            // 
+            this.Time.Text = "Time";
+            this.Time.Width = 59;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(627, 667);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Generer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1328, 702);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Rynkeby Manager";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.ColumnHeader nothing;
+        private System.Windows.Forms.ColumnHeader Event;
+        private System.Windows.Forms.ColumnHeader Currency;
+        private System.Windows.Forms.ColumnHeader Amount;
+        private System.Windows.Forms.ColumnHeader Dateandtime;
+        private System.Windows.Forms.ColumnHeader Customername;
+        private System.Windows.Forms.ColumnHeader Phonenumber;
+        private System.Windows.Forms.ColumnHeader Comment;
+        private System.Windows.Forms.ColumnHeader Transactionid;
+        private System.Windows.Forms.ColumnHeader Transferid;
+        private System.Windows.Forms.ColumnHeader PaymentPoint;
+        private System.Windows.Forms.ColumnHeader Myshopnumber;
+        private System.Windows.Forms.ColumnHeader Bankaccount;
+        private System.Windows.Forms.ColumnHeader Date;
+        public System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColumnHeader Time;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic.FileIO;
+
 
 namespace Rynkeby
 {
@@ -19,7 +22,105 @@ namespace Rynkeby
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            PopulateMyListView(listView1);
 
         }
+
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            selectrandoms(listView1,5);
+        }        
+        private void PopulateMyListView(ListView listView1)
+        {
+            // if you call this method multiple times, don't forget to remove the olds rows
+            listView1.Items.Clear();
+
+            var path = @"C:\Universitet\Privat\testfil.csv";
+
+            using (TextFieldParser csvParser = new TextFieldParser(path))
+            {
+                csvParser.CommentTokens = new string[] { "#" };
+                csvParser.SetDelimiters(new string[] { ";" });
+                csvParser.HasFieldsEnclosedInQuotes = true;
+
+
+                //skipper kolloner
+                csvParser.ReadLine();
+
+                while (!csvParser.EndOfData)
+                {
+                    //læser linje og pointer bevaeger sig til næste linje 
+                    string[] fields = csvParser.ReadFields();
+
+                    ListViewItem row = new ListViewItem();
+                    row.SubItems.Add(fields[0]);
+                    row.SubItems.Add(fields[1]);
+                    row.SubItems.Add(fields[2]);
+                    row.SubItems.Add(fields[3]);
+                    row.SubItems.Add(fields[4]);
+                    row.SubItems.Add(fields[5]);
+                    row.SubItems.Add(fields[6]);
+                    row.SubItems.Add(fields[7]);
+                    row.SubItems.Add(fields[8]);
+                    row.SubItems.Add(fields[9]);
+                    row.SubItems.Add(fields[10]);
+                    row.SubItems.Add(fields[11]);
+                    row.SubItems.Add(fields[12]);
+                    row.SubItems.Add(fields[13]);
+
+                    listView1.Items.Add(row);
+
+                }
+            }
+        }
+
+        private void selectrandoms(ListView listView1, int numberOfRandoms)
+        {
+            List<string> Event = new List<string>();
+            List<string> Currency = new List<string>();
+            List<string> Ammount = new List<string>();
+            List<string> DateAndTime = new List<string>();
+            List<string> customerName = new List<string>();
+            List<string> lwitems = new List<string>();
+            List<string> lwitems = new List<string>();
+            List<string> lwitems = new List<string>();
+            List<string> lwitems = new List<string>();
+            List<string> lwitems = new List<string>();
+            List<string> lwitems = new List<string>();
+            List<string> lwitems = new List<string>();
+            List<string> lwitems = new List<string>();
+            List<string> lwitems = new List<string>();
+
+
+            foreach (ListViewItem item in listView1.Items)
+            {
+                lwitems.Add(item.SubItems[0].Text);
+               
+            }
+
+            List<int> indexOfRandom = new List<int>();
+            var random = new Random();
+            var lower = 0;
+            var upper = test.Length;
+
+            for (int k = 0; k < numberOfRandoms; k++)
+            {
+                var rannum = random.Next(lower, upper);
+                indexOfRandom.Add(rannum);
+            }
+
+            foreach (int item in indexOfRandom)
+            {
+                
+            }
+        }
+
+
+     
     }
 }
